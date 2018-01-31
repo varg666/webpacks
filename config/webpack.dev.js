320fa23a0177bac390511fb5a5eaffc8f52ccaad
varg666 <jemy222@hotmail.com>
2018-01-31 11:54:46 +0100

    First commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main:['./src/main.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  devServer: {
+      contentBase: "dist",
+      overlay: true
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/,
+            use: [
+                {
+                    loader: 'style-loader'
+                },
+                {
+                    loader: 'css-loader'
+                }
+            
+                
+            ]
+        },
+      { 
+        test:/\.html$/,
+        use:[
+            {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].html'
+                }
+            },
+            {
+                loader: 'extract-loader'
+
+            },
+            {
+              loader: 'html-loader',
+              options: {
+                attrs: ['img:src']
+              }
+            }
+
+
+        ]
+
+      },
+      {
+          test: /\.(jpg|gif|png)$/,
+          use: [
+              {
+                  loader: 'file-loader',
+                  options: {
+                      name: 'images/[name].[ext]'
+                  }
+              }
+          ]
+      }
+
+    ]
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,15 @@
+require('./index.html');
+require('./main.css');
+
+
+
+
+
+
+
+
+console.log('hello world');
+
+var a = () => {
+    console.log('hello from es6');
+}
