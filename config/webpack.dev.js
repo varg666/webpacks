a45f2027411fb06ed2b945a3f8697cae45d897f3
varg666 <jemy222@hotmail.com>
2018-01-31 13:37:41 +0100

    adding loaders

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,67 +2,62 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main:['./src/main.js']
+    main: ['./src/main.js']
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../dist')
   },
   devServer: {
-      contentBase: "dist",
-      overlay: true
+    contentBase: "dist",
+    overlay: true
   },
   module: {
-    rules: [
-        {
-            test: /\.css$/,
-            use: [
-                {
-                    loader: 'style-loader'
-                },
-                {
-                    loader: 'css-loader'
-                }
-            
-                
-            ]
-        },
-      { 
-        test:/\.html$/,
-        use:[
-            {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].html'
-                }
-            },
-            {
-                loader: 'extract-loader'
-
-            },
-            {
-              loader: 'html-loader',
-              options: {
-                attrs: ['img:src']
-              }
-            }
-
-
-        ]
-
+    rules: [{
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader'
+        }],
+        exclude: /node_modules/
       },
       {
-          test: /\.(jpg|gif|png)$/,
-          use: [
-              {
-                  loader: 'file-loader',
-                  options: {
-                      name: 'images/[name].[ext]'
-                  }
-              }
-          ]
+        test: /\.css$/,
+        use: [{
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [{
+            loader: 'file-loader',
+            options: {
+              name: '[name].html'
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|git|png)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'images/[name].[ext]'
+          }
+        }]
       }
-
     ]
   }
 }
